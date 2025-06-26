66e5cd767b6c9b8250c15b3833ec409a86acac03
trangwilliam <TrangW@metro.net>
2025-06-26 10:56:56 -0700

    Update create_reports.py

--- scripts/create_reports.py
+++ scripts/create_reports.py
@@ -162,7 +162,7 @@ def create_all_reports() -> None:
     Creates and saves all daily reports relating to COVID-19
     """
 
-    load_dotenv('../env')
+    print(load_dotenv('../.env'))
 
     user = os.getenv("METRO_EMAIL")
     driver = powerbi_login(user) 
